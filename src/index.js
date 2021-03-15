a1a41a0139f7f84138ca8071d2a5dd68827fb70b
arteemm <arteemmarteemm@gmail.com>
2021-03-15 09:49:20 +0300

    refactor: index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 
 exports.min = function min (array) {
-  return 0;
+    
+   if (array && array.length !== 0){
+    array.sort(function(x, y) { return x - y});
+    return array[0];
+        
+   }else { 
+    return 0;
+   }
 }
 
 exports.max = function max (array) {
-  return 0;
+
+    if (array && array.length !== 0){
+    array.sort(function(x, y) { return x - y});
+    return array[array.length-1]; 
+   } else { 
+    return 0;
+    }
+  
 }
 
 exports.avg = function avg (array) {
+
+    if (array && array.length !== 0){
+        let sum = 0;
+        for (let i = 0; i <= array.length-1; i++){
+            sum += array[i];
+        } 
+
+          return (sum / array.length);
+    } else {
   return 0;
+}
 }
